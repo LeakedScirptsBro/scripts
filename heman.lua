ea357fa7f71085b42aaa85ca551c61e88f19cd45
fal <joeybatesissocool@gmail.com>
2022-06-28 10:31:23 -0500

    Update heman.lua

--- heman.lua
+++ heman.lua
@@ -1,1 +1,95 @@
-local l = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/' local g = loadstring(game:HttpGet(l .. 'g.lua'))() local k = loadstring(game:HttpGet(l .. 'addons/k.lua'))() local b = loadstring(game:HttpGet(l .. 'addons/b.lua'))() local a = g:CreateWindow({ Title = 'HE-MAN SIM + BATTLE ARENA', Center = true,  AutoShow = true, }) local s = { Main = a:AddTab('Main'),  ['UI Settings'] = a:AddTab('UI Settings'), } local q = s.Main:AddLeftGroupbox('XP') q:AddSlider('MySlider', { Text = '', Default = 0, Min = 0, Max = 99999, Rounding = 1, Compact = false,  }) local i = Options.MySlider.Value Options.MySlider:OnChanged(function() print('MySlider was changed! New value:', Options.MySlider.Value) end) Options.MySlider:SetValue(3) local p = q:AddButton('Give XP', function() local t = game.Players.LocalPlayer.PlayerGui.RebirthGui.Dialogue.RebirthButton local c = Options.MySlider.Value game:GetService("\82\101\112\108\105\99\97\116\101\100\83\116\111\114\97\103\101").Packages._Index["\115\108\101\105\116\110\105\99\107\95\107\110\105\116\64\49\46\52\46\52"].knit.Services.WeaponService.RE.WeaponUsed:FireServer(c) local m = {"\77\111\117\115\101\66\117\116\116\111\110\49\67\108\105\99\107", "\77\111\117\115\101\66\117\116\116\111\110\49\68\111\119\110", "\65\99\116\105\118\97\116\101\100"} for i,v in pairs(m) do for i,v in pairs(getconnections(t[v])) do v:Fire() end end end) g:SetWatermarkVisibility(true) g:SetWatermark('Mags Hub') g.KeybindFrame.Visible = true; g:OnUnload(function() print('Unloaded!') g.Unloaded = true end) local j = s['UI Settings']:AddLeftGroupbox('Menu') j:AddButton('Unload', function() g:Unload() end) j:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' })  g.ToggleKeybind = Options.MenuKeybind  k:SetLibrary(g) b:SetLibrary(g) b:IgnoreThemeSettings()  b:SetIgnoreIndexes({ 'MenuKeybind' })  k:SetFolder('MyScriptHub') b:SetFolder('MyScriptHub/specific-game') b:BuildConfigSection(s['UI Settings'])  k:ApplyToTab(s['UI Settings']) 
+local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
+
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+
+local Window = Library:CreateWindow({
+	Title = 'HE-MAN SIM + BATTLE ARENA',
+	Center = true,
+	AutoShow = true,
+})
+
+local Tabs = {
+	Main = Window:AddTab('Main'), 
+	['UI Settings'] = Window:AddTab('UI Settings'),
+}
+
+local LeftGroupBox = Tabs.Main:AddLeftGroupbox('XP')
+
+LeftGroupBox:AddSlider('MySlider', {
+	Text = '',
+
+	Default = 0,
+	Min = 0,
+	Max = 99999,
+	Rounding = 1,
+
+	Compact = false, -- If set to true, then it will hide the label
+})
+
+
+local Number = Options.MySlider.Value
+Options.MySlider:OnChanged(function()
+	print('MySlider was changed! New value:', Options.MySlider.Value)
+end)
+
+Options.MySlider:SetValue(3)
+
+local MyButton = LeftGroupBox:AddButton('Give XP', function()
+	local button = game.Players.LocalPlayer.PlayerGui.RebirthGui.Dialogue.RebirthButton
+	local ohNumber1 = Options.MySlider.Value
+
+	game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.4.4"].knit.Services.WeaponService.RE.WeaponUsed:FireServer(ohNumber1)
+	local events = {"MouseButton1Click", "MouseButton1Down", "Activated"}
+	for i,v in pairs(events) do
+		for i,v in pairs(getconnections(button[v])) do
+			v:Fire()
+		end
+	end
+end)
+
+
+
+
+
+
+
+Library:SetWatermarkVisibility(true)
+
+Library:SetWatermark('Mags Hub')
+
+Library.KeybindFrame.Visible = true;
+
+Library:OnUnload(function()
+	print('Unloaded!')
+	Library.Unloaded = true
+end)
+
+local MenuGroup = Tabs['UI Settings']:AddLeftGroupbox('Menu')
+
+MenuGroup:AddButton('Unload', function() Library:Unload() end)
+MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' })
+
+Library.ToggleKeybind = Options.MenuKeybind -- Allows you to have a custom keybind for the menu
+
+
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+
+SaveManager:IgnoreThemeSettings()
+
+SaveManager:SetIgnoreIndexes({ 'MenuKeybind' })
+
+-- use case for doing it this way:
+-- a script hub could have themes in a global folder
+-- and game configs in a separate folder per game
+ThemeManager:SetFolder('MyScriptHub')
+SaveManager:SetFolder('MyScriptHub/specific-game')
+
+-- Builds our config menu on the right side of our tab
+SaveManager:BuildConfigSection(Tabs['UI Settings'])
+
+-- Builds our theme menu (with plenty of built in themes) on the left side
+-- NOTE: you can also call ThemeManager:ApplyToGroupbox to add it to a specific groupbox
+ThemeManager:ApplyToTab(Tabs['UI Settings'])
